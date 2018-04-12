089bfc75d7755d81e72581cfbd777514b77d16c2
narendrasinghrathore <rathoreofficialemail@gmail.com>
2018-04-12 10:43:28 +0530

    Revert "Revert "Demo og singleton class""
    This reverts commit 0a2374f60c57c8cf91036d03c9726cdabb86ad1f.

---
+++ ConsoleApplication/ConsoleApplication/SingleTon/SingletonClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.SingleTon
+{
+    public sealed class SingletonClass
+    {
+        private static SingletonClass instance = null;
+        private static int objectCreation_ = 0;
+        private SingletonClass()
+        {
+            objectCreation_++;
+        }
+
+        public static SingletonClass GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonClass();
+                return instance;
+            }
+        }
+
+        public static int TotalObjectCreations
+        {
+            get
+            {
+                return objectCreation_;
+            }
+            private set { }
+        }
+
+        public void PrintName(string name)
+        {
+            Console.WriteLine("Name: {0}", name);
+        }
+
+
+    }
+}
